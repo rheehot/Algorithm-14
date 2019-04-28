6b5631c3e9d4fc33b127fe575322807c58f1fe5a
SIASIA\siasia1 <cynxia90@gmail.com>
2019-04-29 00:27:53 +0900

    mim
    fibonacci sequence by iteration

---
+++ fibonacci_iteration.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int fibonacci(int n);
+
+int main()
+{
+	int n;
+
+	puts("input number");
+	scanf("%d", &n);
+
+	printf("fibonacci number is : %d\n", fibonacci(n));
+	return 0;
+}
+
+int fibonacci(int n)
+{
+	int F[n + 1], i;
+	F[0] = 0; F[1] = 1;
+
+	for(i = 2; i <= n; i++)
+		F[i] = F[i-1] + F[i-2];
+
+	return (F[n]);
+}
