7cfa083de0b0598a75f159d6e14187d1825f98de
SIASIA\siasia1 <cynxia90@gmail.com>
2019-04-29 00:17:57 +0900

    mim
    make fibonacci sequence by reculsion

---
+++ fibonacci_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fibonacci(int n);
+
+int main()
+{
+	int n;
+
+	puts("put number");
+	scanf("%d", &n);
+	printf("fibonacci number is : %d\n", fibonacci(n));
+	return 0;
+}
+
+int fibonacci(int n)
+{
+	if (n <= 1)
+		return n;
+	return (fibonacci(n-1) + fibonacci(n-2));
+}
